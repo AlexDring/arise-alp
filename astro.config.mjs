3e90a2162aa67e695999cd7f43090f9d48d2b76a
Alex Dring <alexdringppc@gmail.com>
2024-08-31 20:31:54 +0100

    setup ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import storyblok from "@storyblok/astro";
 // import react from "@astrojs/react";
-import { loadEnv } from 'vite';
+// import { loadEnv } from 'vite';
+import netlify from "@astrojs/netlify/functions";
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
       components: {
         // Add your components here
         featured: "components/Featured",
