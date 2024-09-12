b124a277e6b0cba79316799e665836a30bf79ea9
Alex Dring <alexdringppc@gmail.com>
2024-09-12 16:12:40 +0100

    cms setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,29 @@
 import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  image: {
+    domains: ["storyblok.com"],
+  },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  integrations: [
+  storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      config: 'storyblok/Config',
+      featured: 'components/Featured',
+      highlighted: 'components/Highlighted',
+      hero: 'components/Hero',
+    },
+  })]
 });
