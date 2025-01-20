fe7dfa606ebf68bdbf3f2b946674bcab3d4c5fb9
Alex Dring <alexdringppc@gmail.com>
2025-01-20 00:02:44 +0000

    stop using netlify image cdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import storyblok from '@storyblok/astro'
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 import { loadEnv } from 'vite'
 const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  storyblok({ 
+  storyblok({
     accessToken: env.STORYBLOK_TOKEN,
     bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
     components: {
@@ -30,5 +30,7 @@ export default defineConfig({
       }
     }
   }),
-  adapter: netlify()
+  adapter: netlify({
+    imageCDN: false,
+  })
 });
