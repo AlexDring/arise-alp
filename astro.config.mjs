88d06802cc9c479eb9f27022ce650051f74b07de
Alex Dring <alexdringppc@gmail.com>
2025-01-22 16:44:41 +0000

    clean config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import storyblok from '@storyblok/astro'
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify'
 import { loadEnv } from 'vite'
-const env = loadEnv("", process.cwd(), 'STORYBLOK')
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  storyblok({
-    accessToken: env.STORYBLOK_IS_PREVIEW === 'yes' 
-    ? env.STORYBLOK_PREVIEW_TOKEN 
-    : env.STORYBLOK_PUBLIC_TOKEN,
-    bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
-    components: {
-      page: 'storyblok/Page',
-      config: 'storyblok/Config',
-      featured: 'components/Featured',
-      highlighted: 'components/Highlighted',
-      hero: 'components/Hero',
-      banner: 'components/Banner',
-      hero_gallery: 'components/Hero-Gallery',
-    },
-  })],
-  output: env.STORYBLOK_IS_PREVIEW === 'yes' 
-  ? 'server' 
-: 'static',
+    storyblok({
+      accessToken:
+        env.STORYBLOK_IS_PREVIEW === 'yes'
+          ? env.STORYBLOK_PREVIEW_TOKEN
+          : env.STORYBLOK_PUBLIC_TOKEN,
+      bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
+      components: {
+        page: 'storyblok/Page',
+        config: 'storyblok/Config',
+        featured: 'components/Featured',
+        highlighted: 'components/Highlighted',
+        hero: 'components/Hero',
+        banner: 'components/Banner',
+        hero_gallery: 'components/Hero-Gallery'
+      }
+    })
+  ],
+  output: env.STORYBLOK_IS_PREVIEW === 'yes' ? 'server' : 'static',
   ...(import.meta.env.MODE === 'development' && {
     vite: {
       plugins: [basicSsl()],
@@ -36,7 +36,7 @@ export default defineConfig({
   }),
   ...(env.STORYBLOK_IS_PREVIEW === 'yes' && {
     adapter: netlify({
-      imageCDN: false,
+      imageCDN: false
     })
   })
-});
+})
